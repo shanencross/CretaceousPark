b85ca0ae0abae3eb9e8dbcf6d8892725de39942d
Shanen Cross <shanencross@yahoo.com>
2021-06-20 15:27:44 -0700

    remove swagger config
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- CretaceousPark/Startup.cs
+++ CretaceousPark/Startup.cs
@@ -35,8 +35,6 @@ namespace CretaceousPark {
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
       if (env.IsDevelopment()) {
         app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CretaceousPark v1"));
       }
 
       // app.UseHttpsRedirection();
