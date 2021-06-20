cb69bed0691d6de89044705abfd75bfe44132d06
Shanen Cross <shanencross@yahoo.com>
2021-06-20 16:27:22 -0700

    Add Update and Delete routes
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -3,31 +3,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CretaceousPark.Models;
+using System.Linq;
 
-namespace CretaceousPark.Controllers
-{
+
+namespace CretaceousPark.Controllers {
   [Route("api/[controller]")]
   [ApiController]
-  public class AnimalsController : ControllerBase
-  {
+  public class AnimalsController : ControllerBase {
     private readonly CretaceousParkContext _db;
 
-    public AnimalsController(CretaceousParkContext db)
-    {
+    public AnimalsController(CretaceousParkContext db) {
       _db = db;
     }
 
     // GET api/animals
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Animal>>> Get()
-    {
+    public async Task<ActionResult<IEnumerable<Animal>>> Get() {
       return await _db.Animals.ToListAsync();
     }
 
     // POST api/animals
     [HttpPost]
-    public async Task<ActionResult<Animal>> Post(Animal animal)
-    {
+    public async Task<ActionResult<Animal>> Post(Animal animal) {
       _db.Animals.Add(animal);
       await _db.SaveChangesAsync();
 
@@ -36,16 +33,53 @@ namespace CretaceousPark.Controllers
 
     // GET: api/Animals/5
     [HttpGet("{id}")]
-    public async Task<ActionResult<Animal>> GetAnimal(int id)
-    {
-        var animal = await _db.Animals.FindAsync(id);
+    public async Task<ActionResult<Animal>> GetAnimal(int id) {
+      var animal = await _db.Animals.FindAsync(id);
 
-        if (animal == null)
-        {
-            return NotFound();
+      if (animal == null) {
+        return NotFound();
+      }
+
+      return animal;
+    }
+    // PUT: api/Animals/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal) {
+      if (id != animal.AnimalId) {
+        return BadRequest();
+      }
+
+      _db.Entry(animal).State = EntityState.Modified;
+
+      try {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException) {
+        if (!AnimalExists(id)) {
+          return NotFound();
         }
+        else {
+          throw;
+        }
+      }
 
-        return animal;
+      return NoContent();
+    }
+    private bool AnimalExists(int id) {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+    // DELETE: api/Animals/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id) {
+      var animal = await _db.Animals.FindAsync(id);
+      if (animal == null) {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
     }
   }
 }
