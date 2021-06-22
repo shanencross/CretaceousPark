acb11e6eab0994737bf79f30c98c29ff8c20c0a1
Shanen Cross <shanencross@yahoo.com>
2021-06-21 18:04:09 -0700

    Replace animals controller with scaffolded controller made with code generation
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -1,85 +1,107 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CretaceousPark.Models;
-using System.Linq;
 
+namespace CretaceousPark.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly CretaceousParkContext _context;
 
-namespace CretaceousPark.Controllers {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class AnimalsController : ControllerBase {
-    private readonly CretaceousParkContext _db;
-
-    public AnimalsController(CretaceousParkContext db) {
-      _db = db;
-    }
-
-    // GET api/animals
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Animal>>> Get() {
-      return await _db.Animals.ToListAsync();
-    }
-
-    // POST api/animals
-    [HttpPost]
-    public async Task<ActionResult<Animal>> Post(Animal animal) {
-      _db.Animals.Add(animal);
-      await _db.SaveChangesAsync();
-
-      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
-    }
-
-    // GET: api/Animals/5
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Animal>> GetAnimal(int id) {
-      var animal = await _db.Animals.FindAsync(id);
-
-      if (animal == null) {
-        return NotFound();
-      }
-
-      return animal;
-    }
-    // PUT: api/Animals/5
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, Animal animal) {
-      if (id != animal.AnimalId) {
-        return BadRequest();
-      }
-
-      _db.Entry(animal).State = EntityState.Modified;
-
-      try {
-        await _db.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException) {
-        if (!AnimalExists(id)) {
-          return NotFound();
+        public AnimalsController(CretaceousParkContext context)
+        {
+            _context = context;
         }
-        else {
-          throw;
+
+        // GET: api/Animals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Animal>>> GetAnimals()
+        {
+            return await _context.Animals.ToListAsync();
         }
-      }
 
-      return NoContent();
-    }
-    private bool AnimalExists(int id) {
-      return _db.Animals.Any(e => e.AnimalId == id);
-    }
-    // DELETE: api/Animals/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteAnimal(int id) {
-      var animal = await _db.Animals.FindAsync(id);
-      if (animal == null) {
-        return NotFound();
-      }
+        // GET: api/Animals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Animal>> GetAnimal(int id)
+        {
+            var animal = await _context.Animals.FindAsync(id);
 
-      _db.Animals.Remove(animal);
-      await _db.SaveChangesAsync();
+            if (animal == null)
+            {
+                return NotFound();
+            }
 
-      return NoContent();
+            return animal;
+        }
+
+        // PUT: api/Animals/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAnimal(int id, Animal animal)
+        {
+            if (id != animal.AnimalId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(animal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Animals
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Animal>> PostAnimal(Animal animal)
+        {
+            _context.Animals.Add(animal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAnimal", new { id = animal.AnimalId }, animal);
+        }                         // location         // new id                //actual instance of the animal 
+
+        // DELETE: api/Animals/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimal(int id)
+        {
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Animals.Remove(animal);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AnimalExists(int id)
+        {
+            return _context.Animals.Any(e => e.AnimalId == id);
+        }
     }
-  }
 }
