0eae5d9f21d19a29a4b5d86046885cf7eb072c84
Shanen Cross <shanencross@yahoo.com>
2021-06-21 20:44:57 -0700

    Add queryable parameters
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -7,101 +7,104 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CretaceousPark.Models;
 
-namespace CretaceousPark.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AnimalsController : ControllerBase
-    {
-        private readonly CretaceousParkContext _context;
+namespace CretaceousPark.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase {
+    private readonly CretaceousParkContext _context;
 
-        public AnimalsController(CretaceousParkContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Animals
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Animal>>> GetAnimals()
-        {
-            return await _context.Animals.ToListAsync();
-        }
-
-        // GET: api/Animals/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Animal>> GetAnimal(int id)
-        {
-            var animal = await _context.Animals.FindAsync(id);
-
-            if (animal == null)
-            {
-                return NotFound();
-            }
-
-            return animal;
-        }
-
-        // PUT: api/Animals/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAnimal(int id, Animal animal)
-        {
-            if (id != animal.AnimalId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(animal).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AnimalExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Animals
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Animal>> PostAnimal(Animal animal)
-        {
-            _context.Animals.Add(animal);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetAnimal", new { id = animal.AnimalId }, animal);
-        }                         // location         // new id                //actual instance of the animal 
-
-        // DELETE: api/Animals/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAnimal(int id)
-        {
-            var animal = await _context.Animals.FindAsync(id);
-            if (animal == null)
-            {
-                return NotFound();
-            }
-
-            _context.Animals.Remove(animal);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool AnimalExists(int id)
-        {
-            return _context.Animals.Any(e => e.AnimalId == id);
-        }
+    public AnimalsController(CretaceousParkContext context) {
+      _context = context;
     }
+
+    // GET: api/Animalså
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Animal>>> Get(string species, int id, string name, string gender, int age) {
+      var query = _context.Animals.AsQueryable();
+
+      if (species != null) {
+        query = query.Where(entry => entry.Species == species);
+      }
+      if (id != 0) {
+        query = query.Where(entry => entry.AnimalId == id);
+      }
+      if (name != null) {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (gender != null) {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+      if (age != 0) {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+
+      return await query.ToListAsync();
+    }
+
+    // GET: api/Animals/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id) {
+      var animal = await _context.Animals.FindAsync(id);
+
+      if (animal == null) {
+        return NotFound();
+      }
+
+      return animal;
+    }
+
+    // PUT: api/Animals/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAnimal(int id, Animal animal) {
+      if (id != animal.AnimalId) {
+        return BadRequest();
+      }
+
+      _context.Entry(animal).State = EntityState.Modified;
+
+      try {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException) {
+        if (!AnimalExists(id)) {
+          return NotFound();
+        }
+        else {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/Animals
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Animal>> PostAnimal(Animal animal) {
+      _context.Animals.Add(animal);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction("GetAnimal", new { id = animal.AnimalId }, animal);
+    }                         // location      // new id                //actual instance of the animal 
+
+    // DELETE: api/Animals/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id) {
+      var animal = await _context.Animals.FindAsync(id);
+      if (animal == null) {
+        return NotFound();
+      }
+
+      _context.Animals.Remove(animal);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool AnimalExists(int id) {
+      return _context.Animals.Any(e => e.AnimalId == id);
+    }
+  }
 }
+
