87142e3906d6498f7614614bc60717cfdd6591a3
Shanen Cross <shanencross@yahoo.com>
2021-06-21 20:45:16 -0700

    Add validations
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- CretaceousPark/Models/Animal.cs
+++ CretaceousPark/Models/Animal.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 namespace CretaceousPark.Models
+
 {
+    
     public class Animal
     {
         public int AnimalId { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+        [Required]
         public string Species { get; set; }
+        [Range(0, 150, ErrorMessage = "Age must be between 0 and 150.")]    
         public int Age { get; set; }
+        [Required]
         public string Gender { get; set; }
     }
 }
